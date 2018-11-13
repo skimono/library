69cfaa64eae5e01584a6b64e7c610b7c86546d66
Adam Jankowski <WhiteWolf3500@ymail.com>
2018-11-13 20:10:46 +0100

    removed unneeded files

--- src/config/firebase.js
+++
@@ -1,8 +0,0 @@
-var config = {
-  apiKey: "AIzaSyDb6MxVPz02Dzu6TBU6uRMZk6lSfsEuH8E",
-  authDomain: "solwit-pjatk-arc-2018-gr3.firebaseapp.com",
-  databaseURL: "https://solwit-pjatk-arc-2018-gr3.firebaseio.com",
-  projectId: "solwit-pjatk-arc-2018-gr3",
-  storageBucket: "solwit-pjatk-arc-2018-gr3.appspot.com",
-  messagingSenderId: "314895835823"
-};
