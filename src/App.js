52a3326864c8c1c0b8d61421e37eb2f9233d7422
Adam Jankowski <WhiteWolf3500@ymail.com>
2018-11-14 12:35:21 +0100

    clear up

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ import Listing from './Listing'
 import firebase from 'firebase'
 
 var config = {
-//firebase setup
+  apiKey: "AIzaSyDb6MxVPz02Dzu6TBU6uRMZk6lSfsEuH8E",
+  authDomain: "solwit-pjatk-arc-2018-gr3.firebaseapp.com",
+  databaseURL: "https://solwit-pjatk-arc-2018-gr3.firebaseio.com",
+  projectId: "solwit-pjatk-arc-2018-gr3",
+  storageBucket: "solwit-pjatk-arc-2018-gr3.appspot.com",
+  messagingSenderId: "314895835823"
 }
 firebase.initializeApp(config)
 
